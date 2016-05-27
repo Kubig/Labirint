e5bff3b445732dd6e61e21df4b337f346e4a8f0f
Долгих Александр <kubig.blogspot@gmail.com>
2016-05-27 15:17:22 +0300

    end

--- Labirint/Level1.Designer.cs
+++ Labirint/Level1.Designer.cs
@@ -28,16 +28,21 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Level1));
             this.lblStart = new System.Windows.Forms.Label();
             this.lblFinish = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // lblStart
             // 
             this.lblStart.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
             this.lblStart.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.lblStart.Location = new System.Drawing.Point(12, 9);
+            this.lblStart.Location = new System.Drawing.Point(33, 27);
             this.lblStart.Name = "lblStart";
             this.lblStart.Size = new System.Drawing.Size(100, 23);
             this.lblStart.TabIndex = 0;
@@ -48,33 +53,88 @@
             // 
             this.lblFinish.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
             this.lblFinish.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.lblFinish.Location = new System.Drawing.Point(700, 537);
+            this.lblFinish.Location = new System.Drawing.Point(679, 519);
             this.lblFinish.Name = "lblFinish";
             this.lblFinish.Size = new System.Drawing.Size(100, 23);
             this.lblFinish.TabIndex = 1;
             this.lblFinish.Text = "Финиш";
             this.lblFinish.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblFinish.MouseEnter += new System.EventHandler(this.lblFinish_MouseEnter);
             // 
             // label1
             // 
             this.label1.BackColor = System.Drawing.Color.Black;
             this.label1.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(184, 143);
+            this.label1.Location = new System.Drawing.Point(0, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(426, 23);
+            this.label1.Size = new System.Drawing.Size(812, 23);
             this.label1.TabIndex = 2;
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.MouseEnter += new System.EventHandler(this.label5_MouseEnter);
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.Black;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(0, 547);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(812, 23);
+            this.label2.TabIndex = 3;
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label2.MouseEnter += new System.EventHandler(this.label5_MouseEnter);
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.Black;
+            this.label3.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(785, 23);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(27, 524);
+            this.label3.TabIndex = 4;
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label3.MouseEnter += new System.EventHandler(this.label5_MouseEnter);
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.Black;
+            this.label4.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(0, 23);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(27, 524);
+            this.label4.TabIndex = 5;
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label4.MouseEnter += new System.EventHandler(this.label5_MouseEnter);
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.Color.Black;
+            this.label5.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(396, 146);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(27, 278);
+            this.label5.TabIndex = 6;
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label5.MouseEnter += new System.EventHandler(this.label5_MouseEnter);
             // 
             // Level1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Green;
             this.ClientSize = new System.Drawing.Size(812, 569);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.lblFinish);
             this.Controls.Add(this.lblStart);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Level1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "Level1";
+            this.Shown += new System.EventHandler(this.Level1_Show);
             this.ResumeLayout(false);
 
         }
@@ -84,5 +144,9 @@
         private System.Windows.Forms.Label lblStart;
         private System.Windows.Forms.Label lblFinish;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
     }
 }
--- Labirint/Level1.cs
+++ Labirint/Level1.cs
@@ -15,6 +15,45 @@ namespace Labirint
         public Level1()
         {
             InitializeComponent();
+            
+        }
+
+        private void start_game ()
+        {
+            Point point = lblStart.Location;
+            point.Offset(lblStart.Width / 2, lblStart.Height / 2);
+            Cursor.Position = PointToScreen (point);
+            Sound.play_start();
+        }
+
+        private void finish_game()
+        {
+            this.DialogResult = System.Windows.Forms.DialogResult.OK;
+        }
+
+        private void Level1_Show(object sender, EventArgs e)
+        {
+            start_game();
+        }
+
+        private void lblFinish_MouseEnter(object sender, EventArgs e)
+        {
+            finish_game();
+        }
+
+        private void restart_game() // Функция перезапуска игры
+        {
+            Sound.play_fail();
+            DialogResult dr = MessageBox.Show("Выходить за рамки лабиринта нельзя!\n\nБудем ещё играть?", "Вы проиграли!", MessageBoxButtons.YesNo);
+            if (dr == System.Windows.Forms.DialogResult.Yes)
+                start_game();
+            else
+                this.DialogResult = System.Windows.Forms.DialogResult.Abort;
+        }
+
+        private void label5_MouseEnter(object sender, EventArgs e)
+        {
+            restart_game();
         }
     }
 }
